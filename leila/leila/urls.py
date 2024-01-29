802a5bbeb06d671da1d00abf63946a531604c503
Rillary Uchôa <157925432+rillary03@users.noreply.github.com>
2024-01-28 23:59:35 -0300

    - Criei um ambiente de desenvolvimento (.venv) - Adicionei a dependência dj_static para lidar com arquivos estáticos em ambientes de produção - Criei os diretórios 'media' e 'static' para evitar warnings sobre diretórios ausentes"

--- leila/leila/urls.py
+++ leila/leila/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Batatinha/', include('agendaServico.urls'))
+    path('leila/', include('agendaServico.urls'))
 ]
