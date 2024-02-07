0a91dc651a46197216c8ae7a56ea87b33ad33dc3
gleenout <72708046+gleenout@users.noreply.github.com>
2024-02-07 20:40:15 -0300

    Add Bootstrap e JS

---
+++ leila/static/js/script.js
@@ -0,0 +1,36 @@
+/*
+var nome = prompt('Qual o seu nome?');
+const pi = 3.14;
+
+console.log('Nome: '+ nome)
+let idade = prompt('Qual a sua idade?')
+
+function maiorDeIdade(idade){
+    if(idade >= 18){
+    alert("Maior de idade")
+} else {
+    alert("Menor de idade")
+}
+}
+
+for (let i=0; i<3; i++){
+    confirm('Confirme'+i+' vezes.');
+}
+
+switch (nome){
+    case "Murilo":
+        maiorDeIdade(prompt('Qual a sua idade?'));
+        break;
+
+    case "Werlon":
+        alert("Pedir revisão");
+        break;
+
+    case "Grilo":
+        alert("cri cri cri cri cri");
+        break;
+}*/
+
+function mudarTexto(texto){
+    document.getElementById("campo_texto").InnerHTML = "TEXTO";
+}
